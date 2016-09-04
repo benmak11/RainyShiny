7387dac94d285e57ab63d30eb69b13398ad78dea
Ben Makusha <btmakusha@gmail.com>
2016-09-04 11:31:00 -0400

    Updated Cocoapods to the beta version and changed the viewController.swift file to WeatherVC.swift

---
+++ RainyShiny/WeatherVC.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherVC
+//  RainyShiny
+//
+//  Created by Baynham Makusha on 9/4/16.
+//  Copyright © 2016 Ben Makusha. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
